c3e71f4c64da733038af0197e39920e21d5543b4
Valentine Erokhin <a2svior@gmail.com>
2020-04-03 11:51:08 +0300

    Update SignupForm.jsx

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -14,7 +14,7 @@ export const SignupForm = () => {
           </p>
         </div>
         <div className="flex bg-blue-600 py-4 px-6 rounded-full w-full mt-8 md:w-1/2 lg:w-auto">
-        <a className="gumroad-button" href="https://gum.co/lGAcCX" target="_blank" rel="noopener noreferrer">Get a notification when borders open in your country</a>
+        <a className="gumroad-button" href="https://gum.co/lGAcCX" target="_blank" rel="noopener noreferrer">Get notified</a>
 
 
 
